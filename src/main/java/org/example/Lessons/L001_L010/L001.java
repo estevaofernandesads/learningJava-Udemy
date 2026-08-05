18662c14c0542d0d52134b0ada4053a32c274f76
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-05 12:02:53 -0300

    Class001-05-08-2026

---
+++ src/main/java/org/example/Lessons/L001_L010/L001.java
@@ -0,0 +1,13 @@
+package org.example.Lessons.L001_L010;
+
+import java.util.Scanner;
+
+public class L001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        sc.close();
+    }
+}

d499ef12c069ea3532ef8fbabac6b92883670c06
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-06 13:01:35 -0300

    L003

--- src/main/java/org/example/Lessons/L001_L010/L003.java
+++ src/main/java/org/example/Lessons/L001_L010/L003.java
@@ -3,14 +3,16 @@ package org.example.Lessons.L001_L010;
 public class L003 {
     public static void main(String[] args) {
 
-        int x, y;
-
-        x = 5;
+        int x = 5, y, z = 2;
 
         y = 2 * x;
 
         System.out.println(x);
         System.out.println(y);
+
+        double resultado = (double) x / z; // SET THE RESULT TO REAL VALUE WITHOUT DATE
+
+        System.out.printf("Valor do resultado %.2f", resultado);
 
     }
 }

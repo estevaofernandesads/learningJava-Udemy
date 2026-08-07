ac1ce546546837f45270525fc22c7356b72dd128
EstevãoFernandes - Systems Analyst <estevaofernandes1998@gmail.com>
2026-08-06 21:30:15 -0300

    L010

--- src/main/java/org/example/Exercises/E001_E010/E010.java
+++ src/main/java/org/example/Exercises/E001_E010/E010.java
@@ -2,21 +2,17 @@ package org.example.Exercises.E001_E010;
 
 import java.util.Scanner;
 
-public class E009 {
-
+public class E010 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Informe o valor: ");
         int num = sc.nextInt();
 
-        if (num < 0) {
-            System.out.println("NEGATIVO!");
-        } else if (num == 0){
-            System.out.println("ZERO!");
-        }
-        else{
-            System.out.println("NÃO NEGATIVO!");
+        if (num % 2 == 0){
+            System.out.println("PAR!");
+        } else {
+            System.out.println("IMPAR!");
         }
 
         sc.close();

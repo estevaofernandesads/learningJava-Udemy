3c5de10e87cb717526453622a1599c433f64471a
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-07 11:22:56 -0300

    SET L012

--- src/main/java/org/example/Lessons/L011_L020/L012.java
+++ src/main/java/org/example/Lessons/L011_L020/L012.java
@@ -13,6 +13,7 @@ public class L012 {
         System.out.println("Informe o codigo do dia: ");
         code = sc.nextInt();
 
+        /*
         if (code == 1){
             dia = "DOMINGO";
         } else if (code == 2) {
@@ -29,7 +30,47 @@ public class L012 {
             dia = "SABADO";
         } else {
             dia = "VALOR INVALIDO!";
+        } */
+
+        /*
+        switch (code) {
+            case 1:
+                dia = "DOMINGO";
+                break;
+            case 2:
+                dia = "SEGUNDA-FEIRA";
+                break;
+            case 3:
+                dia = "TERÇA-FEIRA";
+                break;
+            case 4:
+                dia = "QUARTA-FEIRA";
+                break;
+            case 5:
+                dia = "QUINTA-FEIRA";
+                break;
+            case 6:
+                dia = "SEXTA-FEIRA";
+                break;
+            case 7:
+                dia = "SABADO";
+                break;
+            default:
+                dia = "VALOR INVALIDO!";
+                break;
         }
+         */
+
+        dia = switch (code) {
+            case 1 -> "DOMINGO";
+            case 2 -> "SEGUNDA-FEIRA";
+            case 3 -> "TERÇA-FEIRA";
+            case 4 -> "QUARTA-FEIRA";
+            case 5 -> "QUINTA-FEIRA";
+            case 6 -> "SEXTA-FEIRA";
+            case 7 -> "SABADO";
+            default -> "VALOR INVALIDO!";
+        };
 
         System.out.printf("Dia da semana: %s", dia);
 

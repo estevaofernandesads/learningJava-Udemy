3fe0967d395cd7e9bf74ffcd72dc4a6f72f16a90
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-07 12:22:22 -0300

    L013

---
+++ src/main/java/org/example/Lessons/L011_L020/L013.java
@@ -0,0 +1,24 @@
+package org.example.Lessons.L011_L020;
+
+import java.util.Scanner;
+
+public class L013 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double preco, desconto;
+
+        System.out.println("Informe o preço do produto: ");
+        preco = sc.nextDouble();
+
+        if (preco < 20.0) {
+            desconto = preco * 0.1;
+        } else {
+            desconto = preco * 0.05;
+        }
+
+        System.out.printf("Desconto de: %.2f", (preco - desconto));
+
+        sc.close();
+    }
+}

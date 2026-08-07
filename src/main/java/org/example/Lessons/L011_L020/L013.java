e580fd62c70546a4c28581d18aae817d9bec4763
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-07 12:29:20 -0300

    SET L013

--- src/main/java/org/example/Lessons/L011_L020/L013.java
+++ src/main/java/org/example/Lessons/L011_L020/L013.java
@@ -6,16 +6,20 @@ public class L013 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double preco, desconto;
+        double preco /*, desconto*/;
 
         System.out.println("Informe o preço do produto: ");
         preco = sc.nextDouble();
 
+        /*
         if (preco < 20.0) {
             desconto = preco * 0.1;
         } else {
             desconto = preco * 0.05;
         }
+        */
+
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
 
         System.out.printf("Desconto de: %.2f", (preco - desconto));
 

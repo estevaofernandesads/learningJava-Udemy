c1198ca50e936c6b9acda11674c2d87d0f30dccc
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-08 07:23:15 -0300

    SET E017

--- src/main/java/org/example/Exercises/E011_E020/E017.java
+++ src/main/java/org/example/Exercises/E011_E020/E017.java
@@ -6,7 +6,15 @@ public class E017 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
+        int senhaValida = 2002, senha = 0;
+
+        while (senha != senhaValida){
+            System.out.println("Informe a senha correta: ");
+            senha = sc.nextInt();
+            System.out.println("Senha Invalida!");
+        }
+
+        System.out.println("Acesso Permitido!");
 
         sc.close();
     }

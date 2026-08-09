14398981f412d4760e36a1f1ed96f84eb81da174
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-09 09:33:44 -0300

    L016

---
+++ src/main/java/org/example/Lessons/L011_L020/L016.java
@@ -0,0 +1,24 @@
+package org.example.Lessons.L011_L020;
+
+import java.util.Scanner;
+
+public class L016 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe quantos números deseja somar: ");
+
+        int quant = sc.nextInt();
+        int num,soma = 0;
+
+        System.out.println("Informe os números que deseja somar: ");
+        for (int i = 0;i < quant; i++){
+            num = sc.nextInt();
+            soma += num;
+        }
+
+        System.out.println(soma);
+
+        sc.close();
+    }
+}

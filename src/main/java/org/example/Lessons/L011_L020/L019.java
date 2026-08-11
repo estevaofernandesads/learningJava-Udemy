ac9ad4608e92beced393918151e7b09eccb54c80
EstevãoFernandes - Systems Analyst <estevaofernandes1998@gmail.com>
2026-08-10 23:18:21 -0300

    PRE-PROJECT L019

---
+++ src/main/java/org/example/Lessons/L011_L020/L019.java
@@ -0,0 +1,13 @@
+package org.example.Lessons.L011_L020;
+
+import java.util.Scanner;
+
+public class L019 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+    }
+}

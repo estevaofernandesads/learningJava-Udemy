8bc32fb8d29c33562b2c4330d3edae9c1b75c139
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-11 16:25:16 -0300

    L019 - Function

--- src/main/java/org/example/Lessons/L011_L020/L019.java
+++ src/main/java/org/example/Lessons/L011_L020/L019.java
@@ -6,8 +6,34 @@ public class L019 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
+        int a, b, c;
+
+        System.out.println("Informe os três números: ");
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        int oMaior = max(a, b,c);
+
+        resultado(oMaior);
 
         sc.close();
+    }
+
+    public static int max(int x, int y, int z){
+        int maior;
+        if (x > y && x > z){
+            maior = x;
+        } else if (y > z){
+            maior = y;
+        } else {
+            maior = z;
+        }
+        return maior;
+    }
+
+    public static void resultado (int valor){
+        System.out.printf("O maior = %d",valor);
     }
 }

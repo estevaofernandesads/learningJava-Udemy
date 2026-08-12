0a5c20d626e1a61e466b5ffce25ed0baa6cb3c72
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-11 21:42:24 -0300

    Product first part

---
+++ src/main/java/org/example/Projects/Produto/Product.java
@@ -0,0 +1,20 @@
+package org.example.Projects.Produto;
+
+public class Product {
+
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock(){
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity){
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity){
+        this.quantity -= quantity;
+    }
+}
---
+++ src/main/java/org/example/Projects/Produto/Program.java
@@ -0,0 +1,26 @@
+package org.example.Projects.Produto;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Product p1 = new Product();
+
+        System.out.println("Informe os dados do produto: ");
+        System.out.println("Informe o nome do produto: ");
+        p1.name = sc.nextLine();
+        System.out.println("Informe o preço do produto: ");
+        p1.price = sc.nextDouble();
+        System.out.println("Informe a quantidade do produto: ");
+        p1.quantity = sc.nextInt();
+
+        System.out.println(p1.name + p1.price + p1.quantity);
+
+
+
+
+        sc.close();
+    }
+}

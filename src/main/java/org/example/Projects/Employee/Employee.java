7ca9aa476bba5cf4c9c265ab36067595d0bf64b5
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-12 18:57:44 -0300

    I.Employee

---
+++ src/main/java/org/example/Projects/Employee/Employee.java
@@ -0,0 +1,19 @@
+package org.example.Projects.Employee;
+
+public class Employee {
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    public double netSalary(){
+        return grossSalary - tax;
+    }
+
+    public void increaseSalary(double percentage){
+        grossSalary += grossSalary * (percentage / 100);
+    }
+
+    public String toString(){
+        return String.format("Funcionario: %s, $ %.2f%n", name, netSalary());
+    }
+}
---
+++ src/main/java/org/example/Projects/Employee/Program.java
@@ -0,0 +1,29 @@
+package org.example.Projects.Employee;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Employee emp = new Employee();
+
+        System.out.println("Nome: ");
+        emp.name = sc.nextLine();
+        System.out.println("Salario solido: ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.println("Taxa: ");
+        emp.tax = sc.nextDouble();
+
+        System.out.println();
+        System.out.print(emp);
+        System.out.println();
+        System.out.print("Qual a porcentagem para aumentar o salario? ");
+        double percentage = sc.nextDouble();
+        emp.increaseSalary(percentage);
+
+        System.out.println();
+        System.out.printf("Atualização de dados %s", emp);
+
+        sc.close();
+    }
+}

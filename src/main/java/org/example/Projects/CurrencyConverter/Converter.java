aefec3811e8c277995ee7226449aae0e692c3d08
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-14 11:44:47 -0300

    Converter

--- src/main/java/org/example/Projects/CurrencyConverter/Converter.java
+++ src/main/java/org/example/Projects/CurrencyConverter/Converter.java
@@ -1,10 +1,12 @@
 package org.example.Projects.CurrencyConverter;
 
 public class Converter {
-    public static double price;
-    public static double dollars;
+    public static double precoDolar;
+    public static double quantidadeDolars;
+    public static final double TAXA =  0.035 ;
     
-
-
+    public static double valorPagar(double precoDolar, double quantidadeDolars){
+        return precoDolar * quantidadeDolars * (1 - TAXA);
+    }
 
 }

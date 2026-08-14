3e76b62f35ef6b73fea4a627116693a643ad60e4
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-14 12:56:36 -0300

    Constriction

--- src/main/java/org/example/Projects/CurrencyConverter/Converter.java
+++ src/main/java/org/example/Projects/CurrencyConverter/Converter.java
@@ -4,7 +4,7 @@ public class Converter {
     public static double precoDolar;
     public static double quantidadeDolars;
     public static final double TAXA =  0.035 ;
-    
+
     public static double valorPagar(double precoDolar, double quantidadeDolars){
         return precoDolar * quantidadeDolars * (1 - TAXA);
     }
---
+++ src/main/java/org/example/Projects/Product_Constructor/Program.java
@@ -0,0 +1,35 @@
+package org.example.Projects.Product_Constructor;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe os dados do produto: ");
+        System.out.println("Informe o nome do produto: ");
+        String name = sc.nextLine();
+        System.out.println("Informe o preço do produto: ");
+        double price = sc.nextDouble();
+        System.out.println("Informe a quantidade do produto: ");
+        int quantity = sc.nextInt();
+
+        ProductConstruct p1 = new ProductConstruct(name, price, quantity);  // CREATES AN OBJECT WITH PREDEFINED ATTRIBUTES
+
+        System.out.printf("Dados do produto: %s%n", p1);
+        System.out.println();
+
+        System.out.println("Produtos adicionados no estoque: ");
+        quantity = sc.nextInt();
+        p1.addProducts(quantity);
+        System.out.printf("Dados atualizados: %s%n", p1);
+        System.out.println();
+
+        System.out.println("Produtos removidos do estoque: ");
+        quantity = sc.nextInt();
+        p1.removeProducts(quantity);
+        System.out.printf("Dados atualizados: %s%n", p1);
+
+        sc.close();
+    }
+}

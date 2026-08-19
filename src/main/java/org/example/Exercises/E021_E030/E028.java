93e1c0ed5c75a0178eb036a8f0926413fec77b31
EstevãoFernandes <estevaofernandes1998@gmail.com>
2026-08-19 11:40:23 -0300

    MODIFY E028 TO E029 AND DO E028

--- src/main/java/org/example/Exercises/E021_E030/E028.java
+++ src/main/java/org/example/Exercises/E021_E030/E028.java
@@ -5,52 +5,31 @@ import java.util.Scanner;
 public class E028 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Quantos números você vai digitar? ");
 
-        System.out.println("Quantidade de pessoas: ");
         int num = sc.nextInt();
-
-        String[] nomes = new String[num];
-        int[] idades = new int[num];
-        double[] alturas = new double[num];
+        double[] reais = new double[num];
         double soma = 0;
 
         for (int i = 0; i < num; i++) {
-            System.out.printf("Dados da %d Pessoa: %n", i+1);
-            sc.nextLine();
-            System.out.print("Name: ");
-            nomes[i]= sc.nextLine();
-            System.out.print("Idade: ");
-            idades[i] = sc.nextInt();
-            System.out.print("Altura: ");
-            alturas[i] = sc.nextDouble();
+            System.out.print("Digite um número: ");
+            reais[i] = sc.nextInt();
+            soma+= reais[i];
         }
-
-        for (int i = 0; i < num; i++) {
-            soma+= alturas[i];
-        }
-
-        double alturaMedia = soma / num;
 
         System.out.println();
-        System.out.printf("Altura média: %.2f%n", alturaMedia);
 
-        int count = 0;
-
+        System.out.print("Valores = ");
         for (int i = 0; i < num; i++) {
-            if (idades[i]<16){
-                count++;
-            }
+            System.out.print(reais[i] + " ");
         }
 
-        double percent = (double) count * 100 / num;
+        System.out.println();
 
-        System.out.printf("Pessoas com menos de 16 anos: %.1f%%%n", percent);
+        double media = soma/num;
 
-        for (int i = 0; i < num; i++) {
-            if (idades[i]<16){
-                System.out.println(nomes[i]);
-            }
-        }
+        System.out.printf("Soma = %.2f%n", soma);
+        System.out.printf("Media = %.2f", media);
 
         sc.close();
     }
---
+++ src/main/java/org/example/Exercises/E021_E030/E029.java
@@ -0,0 +1,57 @@
+package org.example.Exercises.E021_E030;
+
+import java.util.Scanner;
+
+public class E029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantidade de pessoas: ");
+        int num = sc.nextInt();
+
+        String[] nomes = new String[num];
+        int[] idades = new int[num];
+        double[] alturas = new double[num];
+        double soma = 0;
+
+        for (int i = 0; i < num; i++) {
+            System.out.printf("Dados da %d Pessoa: %n", i+1);
+            sc.nextLine();
+            System.out.print("Name: ");
+            nomes[i]= sc.nextLine();
+            System.out.print("Idade: ");
+            idades[i] = sc.nextInt();
+            System.out.print("Altura: ");
+            alturas[i] = sc.nextDouble();
+        }
+
+        for (int i = 0; i < num; i++) {
+            soma+= alturas[i];
+        }
+
+        double alturaMedia = soma / num;
+
+        System.out.println();
+        System.out.printf("Altura média: %.2f%n", alturaMedia);
+
+        int count = 0;
+
+        for (int i = 0; i < num; i++) {
+            if (idades[i]<16){
+                count++;
+            }
+        }
+
+        double percent = (double) count * 100 / num;
+
+        System.out.printf("Pessoas com menos de 16 anos: %.1f%%%n", percent);
+
+        for (int i = 0; i < num; i++) {
+            if (idades[i]<16){
+                System.out.println(nomes[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
